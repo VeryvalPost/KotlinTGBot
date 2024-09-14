23c11aa83b1c5bb53629a716f84177821797f930
veryval <veryval@mail.ru>
2024-09-14 19:40:10 +0300

    New branch for Education Centre. Without GPT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,40 @@
-
 val telegramBotVersion = "6.0.4"
 val url =  "https://jitpack.io"
-plugins {
-    kotlin("jvm") version "1.9.21"
-}
 
+plugins {
+    kotlin("jvm") version "1.8.20"
+}
 
 group = "ru.veryval.kotlinTGBot"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven ("https://jitpack.io")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-// https://mvnrepository.com/artifact/com.github.kotlin-telegram-bot.kotlin-telegram-bot/telegram
     implementation("com.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.4")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
 }
 
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "ru.veryval.kotlinTGBot.ApplicationKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(15)
+    jvmToolchain {
+    }
 }
+
+
---
+++ src/main/kotlin/Contacts.kt
@@ -0,0 +1,9 @@
+package ru.veryval.kotlinTGBot
+
+open class Contacts(val chatId: Long) {
+    var firstName: String = ""
+    val lastName: String = ""
+    var phone: String = ""
+    var stage: String = ""
+    var messID: Long = -1
+}
